a53a2e7b767aaf39e40da00519fc03d46c4913e9
Ahmad1431997 <ahmadabuawed@gmail.com>
2021-05-31 15:29:42 +0300

    recursion

---
+++ recursion.js
@@ -0,0 +1,16 @@
+let numbers = [
+  [1, 2, 3, 4],
+  [3, 6, [5, 6], 8, 2, [2, 4], 9],
+  [4, 2, [6, 7, [2, 6, 1]]],
+];
+
+const sum = (nums) => {
+  let total = 0;
+  nums.map((num) => {
+    if (!isNaN(num)) total += num;
+    else total += sum(num);
+  });
+  return total;
+};
+
+console.log(sum(numbers));
